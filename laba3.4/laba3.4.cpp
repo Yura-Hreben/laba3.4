5888824ca5d39474733a6da4f0e49575b03d0528
yrii <yural1rn@gmail.com>
2022-10-19 15:51:37 +0300

    laba3.4

---
+++ laba3.4/laba3.4.cpp
@@ -0,0 +1,23 @@
+﻿// Lab_03_4.cpp
+// < Гребень Юрій >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 5
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double y; // вхідний параметр
+	double R;
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R = "; cin >> R;
+	// розгалуження в повній формі
+	if ((((y >= 0 && x <= 0) && y <= sqrt(2 * R * R)) || (0 <= y && 0 <= x && y <= sqrt(2 * R * R))) || (y >= 0 && 0 <= x && y <= sqrt(R*R - x*x)))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
